f7df3d8ff82bd19df1ebee7f1e2929aaa5d34a46
Jack Gable <81646353+jack-gable@users.noreply.github.com>
2023-11-30 16:56:31 -0800

    integrated dark/light mode

---
+++ app/js/script.js
@@ -0,0 +1,8 @@
+const radioButtons = document.querySelectorAll(".toggle__wrapper input");
+for (let i = 0; i < radioButtons.length; i++) {
+	radioButtons[i].addEventListener("click", (e) => {
+		document.getElementById("dark").checked
+			? (document.querySelector("body").classList = "dark")
+			: (document.querySelector("body").classList = "light");
+	});
+}
